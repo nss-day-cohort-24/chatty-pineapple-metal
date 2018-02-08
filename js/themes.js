065afe53784f3d4ff70b5c8a628b3c2380ed3bb0
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-02-08 14:07:00 -0600

    changing themes

--- js/themes.js
+++ js/themes.js
@@ -14,18 +14,45 @@ function myFunction() {
   function changeDarkHandler(){
       if(darkCheck.checked) {
         console.log("DARK");
+        let darkTheme = document.getElementById("main-back");
+        darkTheme.className = "darkContainer";
+
+
+        let darkMsg = document.getElementsByClassName("msg");
+        darkMsg.className = "darkMsg";
+
+
+
+        // darkTheme.classList.toggle("darkContainer");
+
+
+
+
+
       }
       else {
           console.log("dark unchecked");
+          let lightTheme = document.getElementById("main-back");
+
+          lightTheme.className = "container";
+
+
       }
     }
 
     function changeLargeHandler(){
         if(largeCheck.checked) {
           console.log("LARGE");
+
+
+
         }
         else {
             console.log("large unchecked");
+
+
+
+
         }
       }
 
