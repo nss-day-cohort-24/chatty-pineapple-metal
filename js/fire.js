da9381c11c1d4eb837d5ef8d2eda3b3ef2fdad9b
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-02-09 15:43:03 -0600

    not mvp things

---
+++ js/fire.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let formMessagesJS = require("./form-messages");
+const emoji = require('emoji-fire');
+
+let inputMessage = document.getElementById("input");
+let chatbox = document.getElementById("chatbox");
+
+let fireFunc = function (event) {
+    event.preventDefault();
+
+    // place the subbmitted message in array
+    let messageArray = formMessagesJS.messages.unshift("🔥🔥🔥");
+    // display array in the dom
+    chatbox.innerHTML += `<div class="msg" id="textbubble"><div class="msg-text"><p>${formMessagesJS.messages[0]}</p></div><button class="delete" id="delete">delete</button></div>`;
+    // clearing input field
+    inputMessage.value = "";
+    // activate clear button
+    document.getElementById("clrButton").disabled = false;
+    // put only 20 messages in output
+    // for (let i = 0; i <= 20; i++){
+    // }
+};
+
+module.exports = { fireFunc };
