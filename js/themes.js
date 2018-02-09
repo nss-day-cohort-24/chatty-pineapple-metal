67750e2486a025425e5570a5db5654db3529bdff
Ben Atkins <benjatkins13@gmail.com>
2018-02-09 12:05:54 -0600

    removed conflict markup

--- js/themes.js
+++ js/themes.js
@@ -13,19 +13,6 @@
 
 
     
-<<<<<<< HEAD
-//   function changeDarkHandler(){
-//       if(darkCheck.checked) {
-//         console.log("DARK");
-//         //Changes background color
-//         let darkTheme = document.getElementById("main-back");
-//         darkTheme.className = "darkContainer";
-
-
-//         //Changes message bubble color
-//         let darkMsg = document.getElementById("msgId");
-//         darkMsg.className = "darkMsg";
-=======
   function changeDarkHandler(){
 
     // CHECKBOX ON
@@ -43,22 +30,12 @@
           darkMsg.item(i).className = "darkMsg";
         }
 
->>>>>>> master
 
 //         // Changes button colors
 
 //         let agroBtnContainer = document.getElementById("agroBtns");
 //         let darkBtn = agroBtnContainer.getElementsByTagName("button");
 
-<<<<<<< HEAD
-//         for (var i = 0; i < darkBtn.length; i++) { 
-//           darkBtn[i].classList.add("darkButton");
-//         }
-
-//         // Changes 'Messeages' heading color
-//         let msgHeader = document.getElementById("heading");
-//         msgHeader.className = "headerDark"
-=======
         for (let i = 0; i < darkBtn.length; i++) { 
           darkBtn[i].classList.add("darkButton");
         }
@@ -66,24 +43,10 @@
         // Changes 'Messeages' heading color
         let msgHeader = document.getElementById("heading");
         msgHeader.className = "headerDark";
->>>>>>> master
 
 
 //         console.log("got iT?");
           
-<<<<<<< HEAD
-
-
-
-//       }
-//       else {
-//           console.log("dark unchecked");
-//           let lightTheme = document.getElementById("main-back");  
-//           lightTheme.className = "container";
-
-//           let darkMsg = document.getElementById("msgId");
-//           darkMsg.className = "msg";
-=======
       }
 
       // CHECKBOX OFF
@@ -97,21 +60,10 @@
           for (let i = darkMsg.length - 1; i >= 0; --i) {
             darkMsg.item(i).className = "msg";
           }
->>>>>>> master
 
 //           let agroBtnContainer = document.getElementById("agroBtns");
 //           let darkBtn = agroBtnContainer.getElementsByTagName("button");
   
-<<<<<<< HEAD
-//           for (var i = 0; i < darkBtn.length; i++) { 
-//             darkBtn[i].classList.remove("darkButton");
-//           }
-
-//           let msgHeader = document.getElementById("heading");
-//           msgHeader.className = "headerLight"
-//       }
-//     }
-=======
           for (let i = 0; i < darkBtn.length; i++) { 
             darkBtn[i].classList.remove("darkButton");
           }
@@ -120,7 +72,6 @@
           msgHeader.className = "headerLight";
       }
     }
->>>>>>> master
 
 //     function changeLargeHandler(){
 //         if(largeCheck.checked) {
