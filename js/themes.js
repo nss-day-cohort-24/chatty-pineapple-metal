6f22ac8918c5f7c514b0c648888771ab4134fd61
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-02-09 09:20:56 -0600

    new button edits

--- js/themes.js
+++ js/themes.js
@@ -16,28 +16,52 @@ function myFunction() {
   function changeDarkHandler(){
       if(darkCheck.checked) {
         console.log("DARK");
+        //Changes background color
         let darkTheme = document.getElementById("main-back");
         darkTheme.className = "darkContainer";
 
+
+        //Changes message bubble color
         let darkMsg = document.getElementById("msgId");
         darkMsg.className = "darkMsg";
 
+        // Changes button colors
 
-        
-        console.log("got tttttthis?");
+        let agroBtnContainer = document.getElementById("agroBtns");
+        let darkBtn = agroBtnContainer.getElementsByTagName("button");
+
+        for (var i = 0; i < darkBtn.length; i++) { 
+          darkBtn[i].classList.add("darkButton");
+        }
+
+        // Changes 'Messeages' heading color
+        let msgHeader = document.getElementById("heading");
+        msgHeader.className = "headerDark"
+
+
+        console.log("got iT?");
           
+
 
 
       }
       else {
           console.log("dark unchecked");
-          let lightTheme = document.getElementById("main-back");
+          let lightTheme = document.getElementById("main-back");  
           lightTheme.className = "container";
 
           let darkMsg = document.getElementById("msgId");
           darkMsg.className = "msg";
 
+          let agroBtnContainer = document.getElementById("agroBtns");
+          let darkBtn = agroBtnContainer.getElementsByTagName("button");
+  
+          for (var i = 0; i < darkBtn.length; i++) { 
+            darkBtn[i].classList.remove("darkButton");
+          }
 
+          let msgHeader = document.getElementById("heading");
+          msgHeader.className = "headerLight"
       }
     }
 
